6757795f66dd35388bcb8faadee502da9f9b8131
Satoshi Takezawa <stakezawa@terrasky.com>
2018-10-03 20:01:49 +0900

    Remove mathjax extension to test if the equation is shown.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,10 +41,8 @@ release = '0.0.1'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.ifconfig',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
+#    'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
